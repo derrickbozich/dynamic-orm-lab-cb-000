7c3c428d0e9f7b20b5566cb19af4fc1d9b43dc52
derrick b <dbozich1@gmail.com>
2018-06-20 07:17:08 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -21,9 +21,7 @@ class InteractiveRecord
     column_names.compact
   end
 
-  self.column_names.each do |column|
-    attr_accessor column.to_sym
-  end
+
 
   def initialize(options={})
     options.each do |key, value|
@@ -36,7 +34,7 @@ class InteractiveRecord
   end
 
   def col_names_for_insert
-    self.class.column_names.delete_if {|column| column = "id"}.join(", ")
+    self.class.column_names.delete_if {|column| column == "id"}.join(", ")
   end
 
   def values_for_insert
@@ -46,6 +44,7 @@ class InteractiveRecord
     end
     values.join(", ")
   end
+
 
 
 end
